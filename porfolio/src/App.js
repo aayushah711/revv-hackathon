9510163d184a164079046079fdd0f7401e3b6899
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-09-16 17:58:22 +0530

    serverSetup

---
+++ porfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AddClient from "./Components/AddClient"
+
+function App() {
+  return (
+    <div>
+      <AddClient />
+    </div>
+  );
+}
+
+export default App;
---
+++ porfolio/src/Components/AddClient.jsx
@@ -0,0 +1,62 @@
+import React from "react"
+import { useState } from "react"
+import { v4 as uuid } from "uuid"
+import axios from "axios"
+
+export default function AddUser() {
+
+    const [name, setName] = useState("")
+    const [cash, setCash] = useState(0)
+    const [manager, setManager] = useState("")
+    const [status, setStatus] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const payload = {
+            id: uuid(),
+            name,
+            capital: {
+                cash: Number(cash),
+                stocks: []
+            },
+            manager1: "manager1"
+        }
+        addClient(payload)
+        console.log(name, cash)
+
+        remove()
+    }
+
+    const handleManager = (e) => {
+        e.preventDefault()
+        setStatus(true)
+    }
+
+    const addClient = (payload) => {
+        axios.post(`http://localhost:3000/clients`, payload)
+    }
+
+    const remove = () => {
+        setName("")
+        setCash(0)
+    }
+
+    return (
+        <div>
+            {
+                status ?
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Enter the Name" value={name} onChange={e => setName(e.target.value)} />
+                        <input type="number" placeholder="Enter the Amount" value={cash} onChange={e => setCash(e.target.value)} />
+                        <input type="submit" value="ADD Client" />
+                    </form>
+                    :
+                    <form onSubmit={handleManager}>
+                        <input type="text" placeholder="Enter the manager Name" value={manager} onChange={e => setManager(e.target.value)} />
+                        <input type="submit" value="confirm" />
+                    </form>
+
+            }
+        </div>
+    )
+}
